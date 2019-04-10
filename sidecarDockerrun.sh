ca8fac515fe44e1d6ae93fb98a59bc223244d31f
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2019-04-10 15:20:48 -0500

    feat(add-fuse-container): updated Dockerfile and script

--- sidecarDockerrun.sh
+++ sidecarDockerrun.sh
@@ -1,1 +1,7 @@
-sleep 5h
+TOKEN_JSON=`curl  http://workspace-token-service.default.svc.cluster.local/token/`
+TOKEN=`jq '.token' $TOKEN_JSON`
+MANIFEST_LIST=`curl https://emalinowskiv1.planx-pla.net/manifests/ -H "Authorization: Bearer $TOKEN"`
+MANIFEST=`jq '.manifests[-1].filename' $MANIFEST_LIST`
+curl https://emalinowskiv1.planx-pla.net/manifests/file/$MANIFEST > ~/manifest.json
+gen3fuse ~/fuse-config.yaml ~/manifest.json /data/fuse emalinowskiv1 https://emalinowskiv1.planx-pla.net https://emalinowskiv1.planx-pla.net 
+#sleep 5h
