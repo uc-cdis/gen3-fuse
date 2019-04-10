c494fb475b94edf6be96776a5d303bf18c4b3730
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2019-04-10 17:57:49 -0500

    feat(add-fuse-container): updated Dockerfile and script

--- sidecarDockerrun.sh
+++ sidecarDockerrun.sh
@@ -1,7 +1,9 @@
-TOKEN_JSON=`curl  http://workspace-token-service.default.svc.cluster.local/token/`
-TOKEN=`jq '.token' $TOKEN_JSON`
-MANIFEST_LIST=`curl https://emalinowskiv1.planx-pla.net/manifests/ -H "Authorization: Bearer $TOKEN"`
-MANIFEST=`jq '.manifests[-1].filename' $MANIFEST_LIST`
-curl https://emalinowskiv1.planx-pla.net/manifests/file/$MANIFEST > ~/manifest.json
-gen3fuse ~/fuse-config.yaml ~/manifest.json /data/fuse emalinowskiv1 https://emalinowskiv1.planx-pla.net https://emalinowskiv1.planx-pla.net 
-#sleep 5h
+TOKEN_JSON=`curl  http://workspace-token-service.default.svc.cluster.local/token/ | jq -r '.token'`
+#TOKEN=`jq --raw-output '.token' $TOKEN_JSON`
+echo $TOKEN_JSON
+MANIFESTEXT=`curl https://emalinowskiv1.planx-pla.net/manifests/ -H "Authorization: Bearer $TOKEN_JSON" | jq --raw-output .manifests[-1].filename`
+echo $MANIFESTEXT
+FILENAME=`echo $MANIFESTEXT | sed 's/\.[^.]*$//'`
+echo $FILENAME
+curl https://emalinowskiv1.planx-pla.net/manifests/file/$MANIFESTEXT -H "Authorization: Bearer $TOKEN_JSON"  > ~/manifest.json
+gen3fuse ~/fuse-config.yaml ~/manifest.json /data/$FILENAME https://emalinowskiv1.planx-pla.net http://workspace-token-service.default
