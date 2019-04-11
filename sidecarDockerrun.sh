45ece37254b8ad7f247a2a2e69c2e9f5f03a1b2a
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2019-04-11 10:04:09 -0500

    feat(add-fuse-container): updated script

--- sidecarDockerrun.sh
+++ sidecarDockerrun.sh
@@ -1,9 +1,15 @@
-while [ true ]
-do
-  TOKEN_JSON=`curl  http://workspace-token-service.default.svc.cluster.local/token/ | jq -r '.token'`
-  MANIFESTEXT=`curl https://emalinowskiv1.planx-pla.net/manifests/ -H "Authorization: Bearer $TOKEN_JSON" | jq --raw-output .manifests[-1].filename`
-  FILENAME=`echo $MANIFESTEXT | sed 's/\.[^.]*$//'`
-  curl https://emalinowskiv1.planx-pla.net/manifests/file/$MANIFESTEXT -H "Authorization: Bearer $TOKEN_JSON"  > ~/manifest.json
-  gen3fuse ~/fuse-config.yaml ~/manifest.json /data/$FILENAME https://emalinowskiv1.planx-pla.net http://workspace-token-service.default
-  sleep 1m
+#!/bin/sh
+while true; do
+    TOKEN_JSON=`curl  http://workspace-token-service.$NAMESPACE/token/ | jq -r '.token'`
+    sleep 1
+    MANIFESTEXT=`curl https://$HOSTNAME/manifests/ -H "Authorization: Bearer $TOKEN_JSON" | jq --raw-output .manifests[-1].filename`
+    if [ $MANIFESTEXT = 'null' ]; then
+        # user doens't have any manifest
+        continue
+    fi
+    FILENAME=`echo $MANIFESTEXT | sed 's/\.[^.]*$//'`
+    if [ ! -d /data/$FILENAME ]; then
+        curl https://$HOSTNAME/manifests/file/$MANIFESTEXT -H "Authorization: Bearer $TOKEN_JSON"  > ~/manifest.json
+        gen3fuse ~/fuse-config.yaml ~/manifest.json /data/$FILENAME https://$HOSTNAME http://workspace-token-service.$NAMESPACE
+    fi
 done
