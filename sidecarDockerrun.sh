b5c2dd2a6cf8199bae5e9a3032a8ee716c1890fe
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2019-04-15 12:18:16 -0500

    fix(fix-sidecar-container): updated sidecar script with small fixes

--- sidecarDockerrun.sh
+++ sidecarDockerrun.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 while true; do
     TOKEN_JSON=`curl  http://workspace-token-service.$NAMESPACE/token/ | jq -r '.token'`
-    sleep 1
-    MANIFESTEXT=`curl https://$HOSTNAME/manifests/ -H "Authorization: Bearer $TOKEN_JSON" | jq --raw-output .manifests[-1].filename`
+    sleep 20
+    MANIFESTEXT=`curl https://$HOSTNAME/manifests/ -H "Authorization: bearer $TOKEN_JSON" | jq --raw-output .manifests[-1].filename`
     if [ $MANIFESTEXT = 'null' ]; then
         # user doens't have any manifest
         continue
@@ -12,4 +12,5 @@ while true; do
         curl https://$HOSTNAME/manifests/file/$MANIFESTEXT -H "Authorization: Bearer $TOKEN_JSON"  > ~/manifest.json
         gen3fuse ~/fuse-config.yaml ~/manifest.json /data/$FILENAME https://$HOSTNAME http://workspace-token-service.$NAMESPACE
     fi
+    sleep 20
 done
