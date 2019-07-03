4f10658cba82fc345f52564d226b2883def7984e
Giang Bui <giangbui0816@gmail.com>
2019-07-03 12:51:51 -0500

    fix(manifest_convert): fix manifest convert func

--- convert.py
+++ convert.py
@@ -5,55 +5,63 @@ from concurrent.futures import ProcessPoolExecutor
 
 
 def merge():
-    dirs = sorted([os.path.join('/data', d) for d in os.listdir('/data') if os.path.isdir(os.path.join('/data', d)) and d != 'data'])
+    dirs = sorted(
+        [
+            os.path.join("/data", d)
+            for d in os.listdir("/data")
+            if os.path.isdir(os.path.join("/data", d)) and d.startswith("manifest")
+        ],
+        key=os.path.getctime,
+    )
     if len(dirs) == 0:
-        print('no manifest is exported')
+        print("No manifest is exported!!!")
         return
     manifest = dirs[-1]
-    
-    path = os.path.join(manifest, 'by-filepath', 'clinical', 'tsv')
-    
-    if os.path.isfile(manifest+'.sync'):
-        print("already converted")
+
+    path = os.path.join(manifest, "by-filepath", "clinical", "tsv")
+
+    if os.path.isfile(manifest + ".sync"):
+        print("The manifest is already converted")
         return
-    
-    output_tsv_folder = os.path.join('/data', '{}_tsv'.format(manifest))
+
+    output_tsv_folder = os.path.join("/data", manifest.replace("manifest", "tsv"))
 
     if not os.path.isdir(output_tsv_folder):
         os.mkdir(output_tsv_folder)
 
-    print('getting latest manifest')    
+    print("Getting the latest manifest")
     print(path)
-    for tbl_name in ['ActionableMutations', 'ICDCode', 'Oncology_Primary', 'Patients']:
+    for tbl_name in ["ActionableMutations", "ICDCode", "Oncology_Primary", "Patients"]:
         print(tbl_name)
         if not os.path.isdir(os.path.join(path, tbl_name)):
-            print("mounting not finished yet")
+            print("Mounting not finished yet")
             return
-        
-        filenames = glob.glob(os.path.join(path, tbl_name, '*.tsv'))
 
-        with open(os.path.join(output_tsv_folder, '{}.tsv'.format(tbl_name)), 'w') as outfile:
+        filenames = glob.glob(os.path.join(path, tbl_name, "*.tsv"))
+
+        with open(
+            os.path.join(output_tsv_folder, "{}.tsv".format(tbl_name)), "w"
+        ) as outfile:
             with ProcessPoolExecutor(max_workers=10) as pool:
                 result = pool.map(readfile, filenames)
                 count = 0
-                first = True
                 for r in result:
                     if count % 100 == 0:
                         print("Processed {} {}".format(count, tbl_name))
                     count += 1
-                    if first:
+                    if not count:
                         outfile.writelines(r)
-                        first = False
                     else:
                         outfile.writelines(r[1:])
-    with open(manifest+'.sync', 'w') as f:
-        f.write("finished")      
+    with open(manifest + ".sync", "w") as f:
+        f.write("Done")
+
 
 def readfile(filename):
     retries = 0
     while True:
         try:
-            with open(filename, 'r') as f:
+            with open(filename, "r") as f:
                 return f.readlines()
         except Exception as e:
             print("Fail to read file {}, retry".format(str(e)))
@@ -63,7 +71,8 @@ def readfile(filename):
                 print("Fail to get file {}".format(filename))
                 return []
 
+
 if __name__ == "__main__":
-    while(True):
+    while True:
         merge()
         time.sleep(1)
