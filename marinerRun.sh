609d29b248153728cc55b7b7bc47dca769693998
mattgarvin1 <mattgarvin1@gmail.com>
2019-09-20 14:31:23 -0500

    Add mariner component flags.

--- marinerRun.sh
+++ marinerRun.sh
@@ -3,23 +3,33 @@
 # maybe use a more vertically scalable method
 # than passing manifest via environment variable
 # Q. What if the manifest is hella big?
-echo $GEN3FUSE_MANIFEST > ~/manifest.json
+echo "writing manifest"
+echo $GEN3FUSE_MANIFEST
+echo $GEN3FUSE_MANIFEST > /manifest.json
 
+echo "running gen3-fuse.."
 gen3-fuse \
 -config=fuse-config.yaml \
 -manifest=manifest.json \
 -mount-point=/$COMMONS_DATA \
 -hostname=https://$HOSTNAME \
--wtsURL=http://workspace-token-service.$GEN3_NAMESPACE \
+-wtsURL=http://workspace-token-service.$GEN3_NAMESPACE
 # >/proc/1/fd/1 2>/proc/1/fd/2 # uncomment after testing
 
 echo "here is the mounted directory:"
 ls -R /$COMMONS_DATA
 
-# $ENGINE_WORKSPACE
-while [[ ! -f /$ENGINE_WORKSPACE/done ]]; do
-  echo "not done"
-done
+if [ $MARINER_COMPONENT == "ENGINE" ]; then
+  echo "waiting for engine to finish.."
+  while [[ ! -f /$ENGINE_WORKSPACE/done ]]; do
+    :
+  done
+else
+  echo "waiting for task to finish.."
+  while [[ ! -f $TOOL_WORKING_DIR\done ]]; do
+    :
+  done
+fi
 
 echo "done, unmounting gen3fuse"
 
