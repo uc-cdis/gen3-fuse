03ff784884f5a2836529e2ef80f2aac3fb8a0c2c
mattgarvin1 <mattgarvin1@gmail.com>
2019-11-07 15:36:31 -0600

    Fix mariner cleanup.

--- marinerRun.sh
+++ marinerRun.sh
@@ -2,14 +2,7 @@
 
 cleanup() {
   killall gen3-fuse
-  cd /$COMMONS_DATA
-  for f in `ls -d`
-  do
-    echo a $f b `pwd`
-    fusermount -uz $f
-    rm -rf $f
-  done
-
+  fusermount -uz /$COMMONS_DATA/data
   exit 0
 }
 
