3ec6cc8bc54eb2b0d621786f632ac196a93cd143
Pauline <ribeyre@uchicago.edu>
2020-03-03 13:24:23 -0600

    feat(token-idp): fix hostname and skip when no manifestservice

--- sidecarDockerrun.sh
+++ sidecarDockerrun.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Only the most recent manifest is mounted.
+# If new manifests are added while the workspace is running,
+# only keep the N latest manifests for each IDP:
+MAX_MANIFESTS=5
+
 cleanup() {
   killall gen3-fuse
   cd /data
@@ -38,21 +43,10 @@ while true; do
         fi
     done
 
-    # TODO remove or format better
-    echo "IDPS: ${IDPS[*]}"
-    echo "BASE_URLS: ${BASE_URLS[*]}"
-
-    for i in "${!IDPS[@]}"; do  # TODO simplify, only loop IDPs once
+    for i in "${!IDPS[@]}"; do
         IDP=${IDPS[$i]}
         BASE_URL=${BASE_URLS[$i]}
         echo "getting manifests for IDP '$IDP' at $BASE_URL"
-
-        # one folder per external IDP, except for "default"
-        IDP_DATA_PATH="/data"
-        if [ $IDP != "default" ]; then
-            DOMAIN=`echo "$BASE_URL" | awk -F/ '{print $3}'`
-            IDP_DATA_PATH="/data/$DOMAIN"
-        fi
 
         resp=`curl $BASE_URL/manifests/ -H "Authorization: bearer ${TOKEN_JSON[$IDP]}" 2>/dev/null`
 
@@ -65,26 +59,32 @@ while true; do
 
         # get the name of the most recent manifest
         MANIFEST_NAME=`echo $resp | jq --raw-output .manifests[-1].filename`
-        if [ $? != 0 || "$MANIFEST_NAME" == "null" ]; then
-            # manifests endpoint did not return JSON (maybe not configured)
-            # or user doesn't have any manifest
+        if [[ $? != 0 ]]; then
+            echo "Manifests endpoints at $BASE_URL/manifests/ did not return JSON. Maybe it's not configured?"
             continue
         fi
-
+        if [[ "$MANIFEST_NAME" == "null" ]]; then
+            # user doesn't have any manifest
+            continue
+        fi
         FILENAME=`echo $MANIFEST_NAME | sed 's/\.[^.]*$//'`
 
-        # gen3-fuse mounts the files in /data/ dir
+        # one folder per IDP
+        DOMAIN=`echo "$BASE_URL" | awk -F/ '{print $3}'`
+        IDP_DATA_PATH="/data/$DOMAIN"
+
+        # gen3-fuse mounts the files in /data/<hostname> dir
         if [ ! -d $IDP_DATA_PATH/$FILENAME ]; then
             echo mount manifest at $IDP_DATA_PATH/$MANIFEST_NAME
             mkdir -p $IDP_DATA_PATH
             curl $BASE_URL/manifests/file/$MANIFEST_NAME -H "Authorization: Bearer ${TOKEN_JSON[$IDP]}" > /manifest.json
-            gen3-fuse -config=/fuse-config.yaml -manifest=/manifest.json -mount-point=$IDP_DATA_PATH/$FILENAME -hostname=https://$HOSTNAME -wtsURL=http://workspace-token-service.$NAMESPACE -wtsIDP=$IDP >/proc/1/fd/1 2>/proc/1/fd/2
+            gen3-fuse -config=/fuse-config.yaml -manifest=/manifest.json -mount-point=$IDP_DATA_PATH/$FILENAME -hostname=$BASE_URL -wtsURL=http://workspace-token-service.$NAMESPACE -wtsIDP=$IDP >/proc/1/fd/1 2>/proc/1/fd/2
         fi
 
-        # get the number of existing manifests. If there are more than 5,
-        # delete the oldest one.
-        if [ $(df $IDP_DATA_PATH/manifest* | sed '1d' | wc -l) -gt 5 ]; then # remove header line
-            OLDDIR=`df $IDP_DATA_PATH/manifest* | grep manifest | cut -d'/' -f 3 | head -n 1`
+        # get the number of existing manifests. If there are more than
+        # MAX_MANIFESTS, delete the oldest one.
+        if [ $(df $IDP_DATA_PATH/manifest* | sed '1d' | wc -l) -gt $MAX_MANIFESTS ]; then # remove header line
+            OLDDIR=`df $IDP_DATA_PATH/manifest* | grep manifest | cut -d'/' -f 4 | head -n 1`
             echo unmount old manifest $OLDDIR
             fusermount -u $IDP_DATA_PATH/$OLDDIR; rm -rf $IDP_DATA_PATH/$OLDDIR
         fi
