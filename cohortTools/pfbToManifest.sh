d16a99f3a0f4ca17bac58f5fbd688f3f1ae06120
Zakir Gowani <zakirgowani@gmail.com>
2020-08-28 16:02:44 -0500

    debugging

--- cohortTools/pfbToManifest.sh
+++ cohortTools/pfbToManifest.sh
@@ -3,9 +3,6 @@
 # Usage:
 # ./pfbToManifest.sh <pfb-filename> <manifest-output-filename>
 
-set -x
-set -v
-
 
 pfb_filename="$1"
 manifest_output_filename="$2"
@@ -15,9 +12,9 @@ if [ ! -f "$pfb_filename" ]; then
     exit 1
 fi
 
-> $manifest_output_filename
+touch "$manifest_output_filename"
 
-files=$(pfb show -i $pfb_filename | grep "object_id")
+files=$(pfb show -i "$pfb_filename" | grep "object_id")
 # files=$(pfb show -i $pfb_filename -n 70 | grep "object_id") # TODO: delete this line
 echo "variable files: $files" # TODO: delete this line
 
@@ -26,21 +23,21 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-echo "[" >> $manifest_output_filename
+echo "[" >> "$manifest_output_filename"
 
-while read record; do
+while read -r record; do
   echo '28 -----'
-  echo $record
-  object_id=$(jq --raw-output .object.object_id <<< $record)
-  echo "{\"object_id\":\"$object_id\"}," >> $manifest_output_filename
+  echo "$record"
+  object_id=$(jq --raw-output .object.object_id <<< "$record")
+  echo "{\"object_id\":\"$object_id\"}," >> "$manifest_output_filename"
 done <<< "$files"
 
-manifest_json=$(sed '$s/,$//' < $manifest_output_filename)
+manifest_json=$(sed '$s/,$//' < "$manifest_output_filename")
 
-echo $manifest_json > $manifest_output_filename
+echo "$manifest_json" > "$manifest_output_filename"
 
-echo "]" >> $manifest_output_filename
+echo "]" >> "$manifest_output_filename"
 
-trimmed_manifest_contents=$(cat $manifest_output_filename | tr -d " \t\n\r")
+trimmed_manifest_contents=$(cat "$manifest_output_filename" | tr -d " \t\n\r")
 
-echo $trimmed_manifest_contents > $manifest_output_filename
+echo "$trimmed_manifest_contents" > "$manifest_output_filename"
