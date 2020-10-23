6d6d82dfa5b5ecd0dd8098eed280fef7a7d9df1a
Zakir Gowani <zakirgowani@gmail.com>
2020-10-23 13:49:38 -0500

    Feat/pfb cohort guid (#29)
    feat: mount files from PFBs to support the Export PFB to Workspace flow

---
+++ cohortTools/pfbToManifest.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Usage:
+# ./pfbToManifest.sh <pfb-filename> <manifest-output-filename>
+
+# This script takes an exported cohort's PFB and parses out object IDs corresponding data files
+# in indexd. The object IDs are outputted in a new manifest.json file of the format
+# [
+#    {
+#        "object_id" : "fbd5b74e-6789-4f42-b88f-f75e72777f5d",
+#    },
+#    ...
+# ]
+
+pfb_filename="$1"
+manifest_output_filename="$2"
+
+if [[ ! -f "$pfb_filename" ]]; then
+    echo "Error: Input file $pfb_filename does not exist."
+    exit 1
+fi
+
+if [[ -f "$manifest_output_filename" ]]; then
+  rm "$manifest_output_filename"
+fi
+touch "$manifest_output_filename"
+
+files=$(pfb show -i "$pfb_filename" | grep "object_id")
+
+if [[ $? -ne 0 ]]; then
+  echo "Parsing $pfb_filename failed. Exiting..."
+  exit 1
+fi
+
+echo "[" >> "$manifest_output_filename"
+
+while read -r record; do
+  object_id=$(jq --raw-output .object.object_id <<< "$record")
+  echo "{\"object_id\":\"$object_id\"}," >> "$manifest_output_filename"
+done <<< "$files"
+
+manifest_json=$(sed '$s/,$//' < "$manifest_output_filename")
+
+echo "$manifest_json" > "$manifest_output_filename"
+
+echo "]" >> "$manifest_output_filename"
+
+trimmed_manifest_contents=$(cat "$manifest_output_filename" | tr -d " \t\n\r")
+
+echo "$trimmed_manifest_contents" > "$manifest_output_filename"
--- sidecarDockerrun.sh
+++ sidecarDockerrun.sh
@@ -29,62 +29,169 @@ trap cleanup SIGTERM
 declare -A TOKEN_JSON  # requires Bash 4
 TOKEN_JSON['default']=$(curl http://workspace-token-service.$NAMESPACE/token/?idp=default 2>/dev/null | jq -r '.token')
 
-while true; do
+run_sidecar() {
+    while true; do
+        # get the list of IDPs the current user is logged into
+        EXTERNAL_OIDC=$(curl http://workspace-token-service.$NAMESPACE/external_oidc/?unexpired=true -H "Authorization: bearer ${TOKEN_JSON['default']}" 2>/dev/null | jq -r '.providers')
+        IDPS=( "default" )
+        BASE_URLS=( "https://$HOSTNAME" )
+        for ROW in $(jq -r '.[] | @base64' <<< ${EXTERNAL_OIDC}); do
+            IDPS+=( $(_jq ${ROW} .idp) )
+            BASE_URLS+=( $(_jq ${ROW} .base_url) )
+        done
 
-    # get the list of IDPs the current user is logged into
-    EXTERNAL_OIDC=$(curl http://workspace-token-service.$NAMESPACE/external_oidc/?unexpired=true -H "Authorization: bearer ${TOKEN_JSON['default']}" 2>/dev/null | jq -r '.providers')
-    IDPS=( "default" )
-    BASE_URLS=( "https://$HOSTNAME" )
-    for ROW in $(jq -r '.[] | @base64' <<< ${EXTERNAL_OIDC}); do
-        IDPS+=( $(_jq ${ROW} .idp) )
-        BASE_URLS+=( $(_jq ${ROW} .base_url) )
+        for i in "${!IDPS[@]}"; do
+            IDP=${IDPS[$i]}
+            BASE_URL=${BASE_URLS[$i]}
+
+            # one folder per IDP
+            DOMAIN=$(awk -F/ '{print $3}' <<< $BASE_URL)
+            IDP_DATA_PATH="/data/$DOMAIN"
+
+            if [ ! -d $IDP_DATA_PATH ]; then
+                mkdir -p $IDP_DATA_PATH
+            fi
+
+            echo "Checking for new exports with IDP '$IDP' at $BASE_URL"
+
+            check_for_new_manifests "$IDP_DATA_PATH" "$NAMESPACE" "$IDP" "$BASE_URL" "$TOKEN_JSON"
+
+            check_for_new_PFB_GUIDs "$IDP_DATA_PATH" "$NAMESPACE" "$IDP" "$BASE_URL" "$TOKEN_JSON"
+
+            # get the number of existing mounted manifests. If there are more than
+            # MAX_MANIFESTS, delete the oldest one.
+            if [ $(df $IDP_DATA_PATH/manifest* | sed '1d' | wc -l) -gt $MAX_MANIFESTS ]; then # remove header line
+                OLDDIR=$(df $IDP_DATA_PATH/manifest* | grep manifest | cut -d'/' -f 4 | head -n 1)
+                echo unmount old manifest $OLDDIR
+                fusermount -u $IDP_DATA_PATH/$OLDDIR; rm -rf $IDP_DATA_PATH/$OLDDIR
+            fi
+        done
+        sleep 10
     done
+}
 
-    for i in "${!IDPS[@]}"; do
-        IDP=${IDPS[$i]}
-        BASE_URL=${BASE_URLS[$i]}
-        echo "getting manifests for IDP '$IDP' at $BASE_URL"
+query_manifest_service() {
+    # This function populates a return value in a variable called $resp
+    URL=$1
 
-        resp=$(curl $BASE_URL/manifests/ -H "Authorization: bearer ${TOKEN_JSON[$IDP]}" 2>/dev/null)
+    resp=$(curl $URL -H "Authorization: bearer ${TOKEN_JSON[$IDP]}" 2>/dev/null)
 
-        # if access token is expired, get a new one and try again
-        if [[ $(jq -r '.error' <<< $resp) =~ 'log' ]]; then
-            echo "get new token for IDP '$IDP'"
-            TOKEN_JSON[$IDP]=$(curl http://workspace-token-service.$NAMESPACE/token/?idp=$IDP 2>/dev/null | jq -r '.token')
-            resp=$(curl $BASE_URL/manifests/ -H "Authorization: bearer ${TOKEN_JSON[$IDP]}" 2>/dev/null)
-        fi
+    # if access token is expired, get a new one and try again
+    if [[ $(jq -r '.error' <<< $resp) =~ 'log' ]]; then
+        echo "Getting new token for IDP '$IDP'"
+        TOKEN_JSON[$IDP]=$(curl http://workspace-token-service.$NAMESPACE/token/?idp=$IDP 2>/dev/null | jq -r '.token')
+        resp=$(curl $URL -H "Authorization: bearer ${TOKEN_JSON[$IDP]}" 2>/dev/null)
+    fi
+}
 
-        # get the name of the most recent manifest
-        MANIFEST_NAME=$(jq --raw-output .manifests[-1].filename <<< $resp)
-        if [[ $? != 0 ]]; then
-            echo "Manifests endpoints at $BASE_URL/manifests/ did not return JSON. Maybe it's not configured?"
-            continue
-        fi
-        if [[ "$MANIFEST_NAME" == "null" ]]; then
-            # user doesn't have any manifest
-            continue
-        fi
-        FILENAME=$(sed 's/\.[^.]*$//' <<< $MANIFEST_NAME)
+mount_manifest() {
+    MANIFEST_NAME=$1
+    IDP_DATA_PATH=$2
+    NAMESPACE=$3
+    IDP=$4
+    BASE_URL=$5
+    TOKEN_JSON=$6
+    PATH_TO_MANIFEST=$7
 
-        # one folder per IDP
-        DOMAIN=$(awk -F/ '{print $3}' <<< $BASE_URL)
-        IDP_DATA_PATH="/data/$DOMAIN"
+    MOUNT_NAME=$(sed 's/\.[^.]*$//' <<< $MANIFEST_NAME)
 
-        # gen3-fuse mounts the files in /data/<hostname> dir
-        if [ ! -d $IDP_DATA_PATH/$FILENAME ]; then
-            echo mount manifest at $IDP_DATA_PATH/$MANIFEST_NAME
-            mkdir -p $IDP_DATA_PATH
-            curl $BASE_URL/manifests/file/$MANIFEST_NAME -H "Authorization: Bearer ${TOKEN_JSON[$IDP]}" > /manifest.json
-            gen3-fuse -config=/fuse-config.yaml -manifest=/manifest.json -mount-point=$IDP_DATA_PATH/$FILENAME -hostname=$BASE_URL -wtsURL=http://workspace-token-service.$NAMESPACE -wtsIDP=$IDP >/proc/1/fd/1 2>/proc/1/fd/2
-        fi
+    # If the manifest is not present locally, we download it
+    if [[ $PATH_TO_MANIFEST == "" ]]; then
+        curl $BASE_URL/manifests/file/$MANIFEST_NAME -H "Authorization: Bearer ${TOKEN_JSON[$IDP]}" > /manifest.json
+        PATH_TO_MANIFEST=/manifest.json
+    fi
 
-        # get the number of existing manifests. If there are more than
-        # MAX_MANIFESTS, delete the oldest one.
-        if [ $(df $IDP_DATA_PATH/manifest* | sed '1d' | wc -l) -gt $MAX_MANIFESTS ]; then # remove header line
-            OLDDIR=$(df $IDP_DATA_PATH/manifest* | grep manifest | cut -d'/' -f 4 | head -n 1)
-            echo unmount old manifest $OLDDIR
-            fusermount -u $IDP_DATA_PATH/$OLDDIR; rm -rf $IDP_DATA_PATH/$OLDDIR
-        fi
-    done
-    sleep 10
-done
+    # gen3-fuse mounts the files in /data/<hostname> dir
+    if [ ! -d $IDP_DATA_PATH/$MOUNT_NAME ]; then
+        echo "Mounting manifest at $IDP_DATA_PATH/$MOUNT_NAME"
+        gen3-fuse -config=/fuse-config.yaml -manifest=$PATH_TO_MANIFEST -mount-point=$IDP_DATA_PATH/$MOUNT_NAME -hostname=$BASE_URL -wtsURL=http://workspace-token-service.$NAMESPACE -wtsIDP=$IDP >/proc/1/fd/1 2>/proc/1/fd/2
+    fi
+}
+
+check_for_new_manifests() {
+    IDP_DATA_PATH=$1
+    NAMESPACE=$2
+    IDP=$3
+    BASE_URL=$4
+    TOKEN_JSON=$5
+
+    resp='' # The below function populates this variable
+    query_manifest_service $BASE_URL/manifests/
+
+    # get the name of the most recent manifest
+    MANIFEST_NAME=$(jq --raw-output .manifests[-1].filename <<< $resp)
+    if [[ $? != 0 ]]; then
+        echo "Manifest service endpoint at $BASE_URL/manifests/ did not return JSON. Maybe it's not configured?"
+        return
+    fi
+    if [[ "$MANIFEST_NAME" == "null" ]]; then
+        # user doesn't have any manifests
+        return
+    fi
+
+    mount_manifest "$MANIFEST_NAME" "$IDP_DATA_PATH" "$NAMESPACE" "$IDP" "$BASE_URL" "$TOKEN_JSON" ""
+}
+
+check_for_new_PFB_GUIDs() {
+    IDP_DATA_PATH=$1
+    NAMESPACE=$2
+    IDP=$3
+    BASE_URL=$4
+    TOKEN_JSON=$5
+
+    resp='' # The below function populates this variable
+    query_manifest_service $BASE_URL/manifests/cohorts
+
+    # Get the GUID of the most recent cohort
+    GUID=$(jq --raw-output .cohorts[-1].filename <<< $resp)
+    if [[ $? != 0 ]]; then
+        echo "Manifest service endpoint at $BASE_URL/manifests/cohorts/ did not return JSON. Maybe it's not configured?"
+        return
+    fi
+    if [[ "$GUID" == "null" || "$GUID" == "" ]]; then
+        # user doesn't have any cohorts
+        return
+    fi
+
+    # Check if this GUID has been mounted already
+    MOUNT_NAME="manifest-$GUID"
+    if [[ -d $IDP_DATA_PATH/$MOUNT_NAME ]]; then
+        return
+    fi
+
+    echo "Got new GUID: $GUID"
+
+    # Now retrieve the contents of the file with this GUID
+    fence_presigned_url_endpoint="$BASE_URL/user/data/download/$GUID"
+    presigned_url_to_cohort_PFB=$(curl $fence_presigned_url_endpoint -H "Authorization: bearer ${TOKEN_JSON[$IDP]}" 2>/dev/null)
+
+    p_url=$(jq --raw-output .url <<< $presigned_url_to_cohort_PFB)
+    if [[ "$p_url" == "null" || "$p_url" == "" || -z $p_url ]]; then
+        echo "Request to Fence endpoint at $BASE_URL/user/data/download/$GUID failed."
+        echo "Error message: $presigned_url_to_cohort_PFB"
+        return
+    fi
+
+    local_filepath_for_cohort_PFB="$IDP_DATA_PATH/cohort-$GUID.avro"
+    curl $p_url --output $local_filepath_for_cohort_PFB
+    if [[ $? != 0 ]]; then
+        echo "Request to presigned URL for cohort PFB at $presigned_url_to_cohort_PFB failed."
+        return
+    fi
+
+    # Next steps: use pyPFB to parse DIDs from the PFB and mount them using gen3-fuse
+    PFB_MANIFEST_NAME="manifest-$GUID.json"
+    pushd /
+    ./pfbToManifest.sh $local_filepath_for_cohort_PFB "$IDP_DATA_PATH/$PFB_MANIFEST_NAME"
+    popd
+    if [[ $? != 0 ]]; then
+        echo "Failed to parse object IDs from $local_filepath_for_cohort_PFB."
+        return
+    fi
+
+    mount_manifest "$PFB_MANIFEST_NAME" "$IDP_DATA_PATH" "$NAMESPACE" "$IDP" "$BASE_URL" "$TOKEN_JSON" "/$IDP_DATA_PATH/$PFB_MANIFEST_NAME"
+
+    rm "/$IDP_DATA_PATH/$PFB_MANIFEST_NAME"
+}
+
+run_sidecar
